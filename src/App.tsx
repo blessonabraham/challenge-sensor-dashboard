f5d75ed21c1889e59a40d5157ed041f72dc9975d
Blesson Abraham <blessonabrm@gmail.com>
2022-08-11 16:51:41 +0530

    Removing Comments

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,4 @@ import { createRoot } from 'react-dom/client'
 import { Store } from './Store/Store';
 import "./App.scss"
 
-// To-DO
-// Caching
-// ReadMe
-// Move text to constants
 createRoot(document.getElementById('root') as HTMLElement).render(<Store/>)
